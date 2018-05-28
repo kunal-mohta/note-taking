ec249165bbf1008b33a0f49444b5c01eae63b68b
kunal-mohta <kunalmohta1818@gmail.com>
2018-05-29 02:13:07 +0530

    express body parse err resolve; connection to mongodb via mongoose(local)

--- server/routers/usersRouter.js
+++ server/routers/usersRouter.js
@@ -1,15 +1,53 @@
+/* Packages */
 const express = require('express');
 const app = express();
 const path = require('path');
 
+/* External files */
+const server = require('../server.js');
+
 const usersRouter = express.Router();
 
-usersRouter.post('/login', function( req, res) {
-  console.log(req);
+usersRouter.post ('/login', (req, res) => {
+  if (!server.dbConnection) {
+    console.log('There is some problem with your database connection... Cannot send request to /users/login');
+  }
+  else {
+    let Users = server.users;
+    
+    console.log(req);
+  }
 });
 
-usersRouter.post('/signup', function (req, res) {
-  console.log(req);
+usersRouter.post ('/signup', (req, res) => {
+  if (!server.dbConnection) {
+    console.log('There is some problem with your database connection... Cannot send request to /users/signup');
+  }
+  else {
+    let Users = server.users;
+
+    Users.findOne(
+      {
+        'username' : req.body.username
+      },
+      (e, user) => {
+        if (user) {
+          console.log('exits');
+        } 
+        else {
+          Users.create(
+            {
+              username: req.body.username,
+              password: req.body.password
+            },
+            (e, user) => {
+              console.log(user);
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
 module.exports = usersRouter;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,65 @@
+/* Packages */
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+//Body parser to parse response body
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const usersRouter = require('./routers/usersRouter.js');
+/* Global Variables */
+let DB_CONNECTION = 0; //boolean to keep track of when connection to db is established
 
+
+/* Module Exports */
+module.exports.dbConnection = DB_CONNECTION;
+
+
+/* Routers */
+const usersRouter = require('./routers/usersRouter.js');
 app.use('/users', usersRouter);
 
-//Starting the express server
-const PORT = 5000;
 
-app.listen(PORT, function (error) {
-  if(error) console.log(error);
+/* Mongoose */
+
+//url of database to connect to (will depend on whether local or global database is being used)
+const dburl = 'mongodb://localhost/note-app';
+
+//Establish connection to the database
+mongoose.connect(dburl);
+
+//Creating db and checking connection
+var db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.log('Error connecting to the database... \n' + error);
+});
+
+//Callback for after connection is established
+db.once('open', () => {
+  console.log('Successful connection to the database...');
+  DB_CONNECTION = 1;
+  module.exports.dbConnection = DB_CONNECTION;
+
+  //User Schema
+  const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+  });
+
+  //User Model
+  const User = mongoose.model('User', userSchema);
+
+  //Exporting the model
+  module.exports.users = User;
+});
+
+
+/* Starting the express server */
+const PORT = 5000;
+app.listen(PORT, (error) => {
+  if (error) console.log(error);
   else console.log(`Express server listening at port ${PORT}`);
 });
